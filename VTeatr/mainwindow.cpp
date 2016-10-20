9dee6f68baa048cdb750eb0585e2a5a16f447e64
Alexander Sergeev <exirtt@gmail.com>
2016-10-20 22:12:28 +0300

    add picture

--- VTeatr/mainwindow.cpp
+++ VTeatr/mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QGraphicsScene * scen = new QGraphicsScene();
+    QPixmap * pix = new QPixmap();
+    pix->load("teatr1.png");
+    scen->addPixmap(*pix);
+    ui->graphicsView->setScene(scen);
 }
 
 MainWindow::~MainWindow()

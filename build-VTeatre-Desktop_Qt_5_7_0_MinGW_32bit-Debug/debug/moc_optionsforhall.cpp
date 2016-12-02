7c2e31fbfbfea857afd4db22a582900bf8b885ee
bahekz <danilryazanov1990@mail.ru>
2016-12-02 16:40:56 +0200

    dfa

--- build-VTeatre-Desktop_Qt_5_7_0_MinGW_32bit-Debug/debug/moc_optionsforhall.cpp
+++ build-VTeatre-Desktop_Qt_5_7_0_MinGW_32bit-Debug/debug/moc_optionsforhall.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_OptionsForHall_t {
-    QByteArrayData data[7];
-    char stringdata0[122];
+    QByteArrayData data[8];
+    char stringdata0[146];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,13 +35,14 @@ QT_MOC_LITERAL(2, 43, 0), // ""
 QT_MOC_LITERAL(3, 44, 4), // "arg1"
 QT_MOC_LITERAL(4, 49, 30), // "on_spinBox_Column_valueChanged"
 QT_MOC_LITERAL(5, 80, 35), // "on_ComboBoxType_currentIndexC..."
-QT_MOC_LITERAL(6, 116, 5) // "index"
+QT_MOC_LITERAL(6, 116, 5), // "index"
+QT_MOC_LITERAL(7, 122, 23) // "on_pushButtonOK_clicked"
 
     },
     "OptionsForHall\0on_spinBox_Row_valueChanged\0"
     "\0arg1\0on_spinBox_Column_valueChanged\0"
     "on_ComboBoxType_currentIndexChanged\0"
-    "index"
+    "index\0on_pushButtonOK_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -51,7 +52,7 @@ static const uint qt_meta_data_OptionsForHall[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -59,14 +60,16 @@ static const uint qt_meta_data_OptionsForHall[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   29,    2, 0x08 /* Private */,
-       4,    1,   32,    2, 0x08 /* Private */,
-       5,    1,   35,    2, 0x08 /* Private */,
+       1,    1,   34,    2, 0x08 /* Private */,
+       4,    1,   37,    2, 0x08 /* Private */,
+       5,    1,   40,    2, 0x08 /* Private */,
+       7,    0,   43,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::Int,    3,
     QMetaType::Void, QMetaType::Int,    3,
     QMetaType::Void, QMetaType::Int,    6,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -80,6 +83,7 @@ void OptionsForHall::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _
         case 0: _t->on_spinBox_Row_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 1: _t->on_spinBox_Column_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 2: _t->on_ComboBoxType_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->on_pushButtonOK_clicked(); break;
         default: ;
         }
     }
@@ -110,13 +114,13 @@ int OptionsForHall::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }

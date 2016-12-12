d5299d92aa2e00cdc9603938817c87b337d9aa05
bahekz <danilryazanov1990@mail.ru>
2016-12-13 01:15:13 +0200

    f

--- VTeatre/mainwindow.cpp
+++ VTeatre/mainwindow.cpp
@@ -10,7 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     mydb = QSqlDatabase::addDatabase("QSQLITE");
-    mydb.setDatabaseName("C:/Cursovaya/VTeatre.sqlite");
+    mydb.setDatabaseName("D:/Cursovaya/VTeatre.sqlite");
 
     CurScene = new Scene(3);
 
